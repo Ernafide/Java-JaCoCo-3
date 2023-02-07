82eb7ba62759fd1a6cbc27107e460ce136efd34d
Julia <Ernafide@gmail.com>
2023-02-07 13:52:13 +0300

    Test

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -16,4 +16,15 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findMaxinMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 18, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 18;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
